3cb0c63795fe24a1eab7e8c3e8511ddeca299a10
ForumLMFI <v-forum.shah@lightfinance.com>
2024-06-10 12:01:12 +0000

    Copilot third commit3

---
+++ comment.js
@@ -0,0 +1,63 @@
+//create addition code 
+// // create a get route 
+// app.get('/about', (req, res) => {
+//     res.send('about');
+// });
+// // create a post route 
+// app.post('/about', (req, res) => {
+//     res.send('Got a POST request');
+// });
+// // create a put route 
+// app.put('/about/user', (req, res) => {
+//     res.send('Got a PUT request at /user');
+// });
+// // create a delete route 
+// app.delete('/about/user', (req, res) => {
+//     res.send('Got a DELETE request at /user');
+// });
+// // create a listen 
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+// 
+// // http://localhost:3000/about
+// // create a get route 
+// app.get('/contact', (req, res) => {
+//     res.send('contact');
+// });
+// // create a post route 
+// app.post('/contact', (req, res) => {
+//     res.send('Got a POST request');
+// });
+// // create a put route 
+// app.put('/contact/user', (req, res) => {
+//     res.send('Got a PUT request at /user');
+// });
+// // create a delete route 
+// app.delete('/contact/user', (req, res) => {
+//     res.send('Got a DELETE request at /user');
+// });
+// // create a listen 
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+// 
+// // http://localhost:3000/contact
+// // create a get route 
+// app.get('/help', (req, res) => {
+//     res.send('help');
+// });
+// // create a post route 
+// app.post('/help', (req, res) => {
+//     res.send('Got a POST request');
+// });
+// // create a put route 
+// app.put('/help/user', (req, res) => {
+//     res.send('Got a PUT request at /user');
+// });
+// // create a delete route 
+// app.delete('/help/user', (req, res) => {
+//     res.send('Got a DELETE request at /user');
+// });
+// // create a listen 
+// app.listen(port, () => {
